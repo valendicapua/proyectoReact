12d8fa388eea485815e806e3fc4cf39beadd3d78
valendicapua <valentindicapua@outlook.com>
2022-05-15 18:53:30 -0300

    .

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+       
+      <h1> Hola</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam odio quidem a unde distinctio adipisci, labore officiis reprehenderit laborum inventore eaque assumenda et possimus aut dolor ipsa iste voluptatibus nostrum.
+
+      </p>
+    </div>
+  );
+}
+
+export default App;
